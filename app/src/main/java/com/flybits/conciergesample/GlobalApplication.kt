dd910724cb0c214d0459d49b61bdc07dda92ec43
Karol Zdebel <karolmzdebel@gmail.com>
2019-06-05 15:43:17 -0400

    adding view provider registration

--- app/src/main/java/com/flybits/conciergesample/GlobalApplication.kt
+++ app/src/main/java/com/flybits/conciergesample/GlobalApplication.kt
@@ -3,6 +3,10 @@ package com.flybits.conciergesample
 import android.annotation.SuppressLint
 import android.app.Application
 import com.flybits.concierge.FlybitsConcierge
+import com.flybits.concierge.smartrewards.viewproviders.BenefitsViewProvider
+import com.flybits.concierge.smartrewards.viewproviders.ConfirmationViewProvider
+import com.flybits.concierge.smartrewards.viewproviders.OffersViewProvider
+import com.flybits.concierge.smartrewards.viewproviders.OptInViewProvider
 
 class GlobalApplication: Application() {
 
@@ -12,6 +16,12 @@ class GlobalApplication: Application() {
         val concierge = FlybitsConcierge.with(this)
         //This needs to happen prior to using API, doesn't need to be in Application.onCreate()
         concierge.initialize(R.xml.concierge)
+
+        //Add view providers for content templates you want displayed
+        concierge.registerFlybitsViewProvider(OptInViewProvider(this))
+        concierge.registerFlybitsViewProvider(OffersViewProvider(this))
+        concierge.registerFlybitsViewProvider(BenefitsViewProvider(this))
+        concierge.registerFlybitsViewProvider(ConfirmationViewProvider(this))
         concierge.enableDebugMode()
 
     }
