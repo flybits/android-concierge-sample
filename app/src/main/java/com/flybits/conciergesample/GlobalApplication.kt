8bae75c03476b4073740ab274f5143cfba63d62c
Abhijit <abhijit.ghangurde@flybits.com>
2019-06-27 18:16:18 -0400

    Added the notification channel creation in MainActivity.

--- app/src/main/java/com/flybits/conciergesample/GlobalApplication.kt
+++ app/src/main/java/com/flybits/conciergesample/GlobalApplication.kt
@@ -2,31 +2,24 @@ package com.flybits.conciergesample
 
 import android.annotation.SuppressLint
 import android.app.Application
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.os.Build
 import com.flybits.concierge.FlybitsConcierge
-import com.flybits.concierge.smartrewards.viewproviders.BenefitsViewProvider
-import com.flybits.concierge.smartrewards.viewproviders.ConfirmationViewProvider
-import com.flybits.concierge.smartrewards.viewproviders.OffersViewProvider
-import com.flybits.concierge.smartrewards.viewproviders.OptInViewProvider
 
-class GlobalApplication: Application() {
+class GlobalApplication : Application() {
 
     @SuppressLint("ResourceType")
     override fun onCreate() {
         super.onCreate()
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            // Create the NotificationChannel
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val mChannel = NotificationChannel(CHANNEL_ID, "Smart Rewards", importance)
-            mChannel.description = "Notifications for smart rewards"
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(mChannel)
-        }
+        /*  if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+              // Create the NotificationChannel
+              val importance = NotificationManager.IMPORTANCE_DEFAULT
+              val mChannel = NotificationChannel("abhi", "Smart Rewards", importance)
+              mChannel.description = "Notifications for smart rewards"
+              // Register the channel with the system; you can't change the importance
+              // or other notification behaviors after this
+              val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+              notificationManager.createNotificationChannel(mChannel)
+          }*/
 
         val concierge = FlybitsConcierge.with(this)
 
@@ -35,9 +28,9 @@ class GlobalApplication: Application() {
         concierge.enableDebugMode()
 
         //Add view providers for content templates you want displayed
-        concierge.registerFlybitsViewProvider(OptInViewProvider(this))
+        /*concierge.registerFlybitsViewProvider(OptInViewProvider(this))
         concierge.registerFlybitsViewProvider(OffersViewProvider(this))
         concierge.registerFlybitsViewProvider(BenefitsViewProvider(this))
-        concierge.registerFlybitsViewProvider(ConfirmationViewProvider(this))
+        concierge.registerFlybitsViewProvider(ConfirmationViewProvider(this))*/
     }
 }
--- app/src/main/java/com/flybits/conciergesample/activity/MainActivity.kt
+++ app/src/main/java/com/flybits/conciergesample/activity/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.flybits.conciergesample.activity
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -10,9 +13,10 @@ import com.flybits.android.push.models.Push
 import com.flybits.concierge.ConciergeConstants
 import com.flybits.concierge.FlybitsConcierge
 import com.flybits.concierge.enums.ShowMode
+import com.flybits.conciergesample.CHANNEL_ID
 import com.flybits.conciergesample.R
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     private var flybitsConcierge: FlybitsConcierge? = null
 
@@ -26,6 +30,9 @@ class MainActivity: AppCompatActivity() {
         flybitsConcierge = FlybitsConcierge.with(applicationContext)
         intent?.let { handleIntent(it) }
 
+        //Create Channel For notification : you can create it anywhere and return the Channel ID in AppMessaging Service
+        createNotificationChannel(CHANNEL_ID, "CustomChannel_name", "hello")
+
     }
 
     override fun onNewIntent(intent: Intent?) {
@@ -34,9 +41,20 @@ class MainActivity: AppCompatActivity() {
     }
 
     private fun handleIntent(intent: Intent) {
-        if (intent.hasExtra(ConciergeConstants.PUSH_EXTRA)){
+        if (intent.hasExtra(ConciergeConstants.PUSH_EXTRA)) {
             val extra = intent.getParcelableExtra<Push>(ConciergeConstants.PUSH_EXTRA)
             flybitsConcierge?.showPush(ShowMode.NEW_ACTIVITY, extra)
         }
     }
+
+    private fun createNotificationChannel(channelId: String, channelName: String, channelDesc: String) {
+        lateinit var channel: NotificationChannel
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
+            channel.description = channelDesc
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
 }
