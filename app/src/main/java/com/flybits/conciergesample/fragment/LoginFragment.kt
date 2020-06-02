f8c367f7c7d2d37c8c7c2210655905f346237b77
Abhijit Ghangurde <51491404+abhijitgh13@users.noreply.github.com>
2020-06-02 00:12:21 -0400

    Added push notificaiton extra in Main Activity which is used in Pushservice to open  and update concierge to 3.3.4-alpha2 with commitAllowStateLoss Change and removed code for un wanted fragment

--- app/src/main/java/com/flybits/conciergesample/fragment/LoginFragment.kt
+++ app/src/main/java/com/flybits/conciergesample/fragment/LoginFragment.kt
@@ -8,20 +8,21 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.flybits.conciergesample.R
 import com.flybits.commons.library.api.idps.AnonymousIDP
-import com.flybits.commons.library.api.idps.FlybitsIDP
+import com.flybits.commons.library.api.idps.JwtIDP
 import com.flybits.commons.library.exceptions.FlybitsException
-import com.flybits.concierge.*
+import com.flybits.concierge.AuthenticationStatusListener
+import com.flybits.concierge.FlybitsConcierge
+import com.flybits.conciergesample.R
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_login.*
 
-class LoginFragment: Fragment(), AuthenticationStatusListener {
+class LoginFragment : Fragment(), AuthenticationStatusListener {
 
     private var concierge: FlybitsConcierge? = null
 
-    companion object{
-        var is2Phase= false
+    companion object {
+        var is2Phase = false
         var username = ""
         var password = ""
     }
@@ -35,7 +36,11 @@ class LoginFragment: Fragment(), AuthenticationStatusListener {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
@@ -46,7 +51,7 @@ class LoginFragment: Fragment(), AuthenticationStatusListener {
         button_login.setOnClickListener {
             progressBar?.visibility = View.VISIBLE
             is2Phase = false
-            concierge?.authenticate(FlybitsIDP(field_email.text.toString(), field_password.text.toString()))
+            concierge?.authenticate(JwtIDP("eyJhbGciOiJIUzI1NiIsImtpZCI6IjAyN0VGQkU3LTAxMjMtNDgyMC04OTQ3LUI4NjM3Q0FCQTgzNSIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1OTYwMDA0OTUsIm5iZiI6MTU5MDgxNjQ5NSwidXNlcklEIjoiODBCQ0M3OUQtRjNGRi00MDlFLTlEMEQtRTkwN0Y1MEI1QTM4IiwiZGV2aWNlSUQiOiJDRjE4MjNGNC05REQxLTQzNkEtQkUxNy00MjdGQjY4NDlEQkQiLCJ0ZW5hbnRJRCI6IjAyN0VGQkU3LTAxMjMtNDgyMC04OTQ3LUI4NjM3Q0FCQTgzNSIsImlzU0EiOmZhbHNlfQ.tatXFCA6Iy4uXvkr7pqIBF_lIwGOyRhYWHzRjMzOXO4"))
         }
         button_login_anonymous.setOnClickListener {
             progressBar?.visibility = View.VISIBLE
@@ -59,19 +64,22 @@ class LoginFragment: Fragment(), AuthenticationStatusListener {
             username = field_email.text.toString()
             password = field_password.text.toString()
 
-            if(username.isNotEmpty() && password.isNotEmpty()) {
+            if (username.isNotEmpty() && password.isNotEmpty()) {
                 progressBar?.visibility = View.GONE
                 is2Phase = true
                 findNavController().navigate(R.id.action_loginFragment_to_accountFragment)
-            }
-            else {
-                Toast.makeText(context,"Please enter username and password to connect",Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(
+                    context,
+                    "Please enter username and password to connect",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
 
         button_two_phase2.setOnClickListener {
-            username=""
-            password=""
+            username = ""
+            password = ""
             progressBar?.visibility = View.GONE
             is2Phase = true
             findNavController().navigate(R.id.action_loginFragment_to_accountFragment)
