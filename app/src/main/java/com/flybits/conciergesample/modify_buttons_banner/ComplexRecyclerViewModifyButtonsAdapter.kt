c05ffa99b165ff54c81915bacdf3a5c7111272ce
Michael Hollis <michael.hollis@flybits.com>
2022-05-31 13:16:16 -0400

    Added new files trying to run as banners before modify

---
+++ app/src/main/java/com/flybits/conciergesample/modify_buttons_banner/ComplexRecyclerViewModifyButtonsAdapter.kt
@@ -0,0 +1,109 @@
+package com.flybits.conciergesample.modify_buttons_banner
+
+import com.flybits.conciergesample.banners.FragmentForModifyBanners
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.flybits.concierge.enums.ConciergeOptions
+import com.flybits.concierge.enums.Container
+import com.flybits.concierge.enums.ContentStyle
+import com.flybits.conciergesample.R
+import com.flybits.conciergesample.modify_buttons_banner.*
+
+class ComplexRecyclerViewModifyButtonsAdapter(private val items: List<Any>, var context: Context) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val Investment = 0
+    private val Concierge = 1
+    private val Savings = 2
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if (items[position] is FragmentForModifyBanners.Investments) {
+            return Investment
+        } else if (items[position] is String) {
+            return Concierge
+        } else if (items[position] is FragmentForModifyBanners.Savings) {
+            return Savings
+        }
+        return -1
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val viewHolder: RecyclerView.ViewHolder
+        val inflater = LayoutInflater.from(viewGroup.context)
+        viewHolder = when (viewType) {
+            Investment -> {
+                val v1: View = inflater.inflate(R.layout.layout_viewholder1, viewGroup, false)
+                ViewHolderModifyBanner1(v1)
+            }
+            Concierge -> {
+                val v2: View = inflater.inflate(R.layout.layout_viewholder2, viewGroup, false)
+                ViewHolderModifyBanners2(v2)
+            }
+            Savings -> {
+                val v2: View = inflater.inflate(R.layout.layout_viewholder3, viewGroup, false)
+                ViewHolderModifyBanners2(v2)
+            }
+            else -> {
+                val v2: View = inflater.inflate(R.layout.layout_viewholder2, viewGroup, false)
+                ViewHolderModifyBanner1(v2)
+            }
+        }
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
+        when (viewHolder.itemViewType) {
+            Investment -> {
+                val vh1 = viewHolder as ViewHolderModifyBanner1
+                configureViewHolder1(vh1, position)
+            }
+            Concierge -> {
+                val vh2 = viewHolder as ViewHolderModifyBanners2
+                configureViewHolder2(vh2, position)
+            }
+            Savings -> {
+                val vh3 = viewHolder as ViewHolderModifyBanners3
+                configureViewHolder3(vh3, position)
+            }
+            else -> {
+
+            }
+        }
+    }
+
+    private fun configureViewHolder1(vh1: ViewHolderModifyBanner1, position: Int) {
+        val investments = items[position] as FragmentForModifyBanners.Investments
+        vh1.label1.text = "Type: ${investments.type}"
+        vh1.label2.text = "Amount: ${investments.amount}"
+    }
+
+    private fun configureViewHolder2(vh2: ViewHolderModifyBanners2, position: Int) {
+        val transaction = (context as AppCompatActivity).supportFragmentManager.beginTransaction()
+        com.flybits.concierge.Concierge.fragment(
+            context,
+            Container.Expose,
+            null,
+            arrayListOf(
+                ConciergeOptions.Style(ContentStyle.BANNER)
+            )
+        ).let {
+            transaction.replace(vh2.frameLayout.id, it)
+        }
+        transaction.commit()
+    }
+
+    private fun configureViewHolder3(vh3: ViewHolderModifyBanners3, position: Int) {
+        val savings = items[position] as FragmentForModifyBanners.Savings
+        vh3.amount_chequing.text = savings.amount_cheq
+        vh3.amount_credit.text = savings.amount_credit
+        vh3.amount_savings.text = savings.amount_savings
+    }
+}
---
+++ app/src/main/java/com/flybits/conciergesample/modify_buttons_banner/FragmentForModifyBanners.kt
@@ -0,0 +1,149 @@
+package com.flybits.conciergesample.banners
+
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.afollestad.materialdialogs.MaterialDialog
+import com.flybits.commons.library.api.results.callbacks.BasicResultCallback
+import com.flybits.commons.library.exceptions.FlybitsException
+import com.flybits.concierge.Concierge
+import com.flybits.conciergesample.R
+import com.flybits.conciergesample.modify_buttons_banner.ComplexRecyclerViewModifyButtonsAdapter
+import com.flybits.flybitscoreconcierge.idps.AnonymousConciergeIDP
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_account.*
+
+class FragmentForModifyBanners: Fragment() {
+    companion object {
+        fun newInstance(): FragmentForBanners {
+            return FragmentForBanners()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_account, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activity?.let { requestPermissions(it) }
+        setHasOptionsMenu(true)
+
+        // Recycler view
+        val adapter = context?.let { ComplexRecyclerViewModifyButtonsAdapter(getSampleArrayList(), it) }
+        rv_common_items.adapter = adapter
+        rv_common_items.layoutManager = LinearLayoutManager(context)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+    }
+
+
+    private fun requestPermissions(activity: Activity) {
+        if (ContextCompat.checkSelfPermission(
+                activity,
+                ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    activity,
+                    ACCESS_FINE_LOCATION
+                )
+            ) {
+
+                MaterialDialog.Builder(activity)
+                    .title("Location Permission")
+                    .positiveText("Ok")
+                    .content("Location helps us deliver content to you when it is most relevant. Enable the location permission to take advantage of this feature.")
+                    .dismissListener {
+                        ActivityCompat.requestPermissions(
+                            activity,
+                            arrayOf(ACCESS_FINE_LOCATION),
+                            LOCATION_PERMISSION_REQUEST
+                        )
+                    }
+                    .build()
+                    .show()
+
+            } else {
+                ActivityCompat.requestPermissions(
+                    activity,
+                    arrayOf(ACCESS_FINE_LOCATION),
+                    LOCATION_PERMISSION_REQUEST
+                )
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.login_flybits) {
+            // Check if connected already.
+            if (!Concierge.isConnected(requireContext())) {
+                // Call Connect and load the fragment.
+                Concierge.connect(requireContext(), AnonymousConciergeIDP())
+                rv_common_items.adapter?.notifyDataSetChanged()
+            } else {
+                Concierge.disconnect(requireContext(), object : BasicResultCallback {
+                    override fun onException(exception: FlybitsException) {
+                        context?.let { _ ->
+                            progressBar.visibility = View.GONE
+                            view?.let {
+                                Snackbar.make(
+                                    it,
+                                    "Error logging out!",
+                                    Snackbar.LENGTH_LONG
+                                ).show()
+                            }
+                        }
+                    }
+
+                    override fun onSuccess() {
+                        context?.let { _ ->
+                            progressBar.visibility = View.GONE
+                            view?.let {
+                                Snackbar.make(it, "Logged out!", Snackbar.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+                })
+            }
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_options, menu)
+    }
+
+
+    private fun getSampleArrayList(): ArrayList<Any> {
+        val items: ArrayList<Any> = ArrayList()
+        items.add(Savings("$4,000","$5,000","10,000"))
+        items.add(Investments("RRSP", "$5000"))
+        items.add(Investments("TFSA", "$10,000"))
+        items.add("Concierge")
+        items.add(Investments("GIC", "$15,5000"))
+        return items
+    }
+
+
+    data class Investments(val type: String, var amount: String)
+
+    data class Savings(val amount_cheq: String, var amount_savings: String, var amount_credit:String)
+}
